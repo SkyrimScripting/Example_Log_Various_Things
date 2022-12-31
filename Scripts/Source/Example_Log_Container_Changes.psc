cb781d42b0a3e67c1022576c7b6accd03f907f11
Mrowr Purr <mrowr.purr@gmail.com>
2022-12-30 16:07:32 -0800

    Demo writing to both Papyrus log and SKSE log file

---
+++ Scripts/Source/Example_Log_Container_Changes.psc
@@ -0,0 +1,13 @@
+scriptName Example_Log_Container_Changes extends Actor
+
+Function Log(string text) global
+
+EndFunction
+
+Event OnItemAdded(Form akBaseItem, int aiItemCount, ObjectReference akItemReference, ObjectReference akSourceContainer)
+    Log("Player OnItemAdded " + akBaseItem.GetFormID())
+EndEvent
+
+Event OnItemRemoved(Form akBaseItem, int aiItemCount, ObjectReference akItemReference, ObjectReference akDestContainer)
+    Log("Player OnItemRemoved " + akBaseItem.GetFormID())
+EndEvent
--- plugin.cpp
+++ plugin.cpp
@@ -1,12 +1,13 @@
 #include <SkyrimScripting/Plugin.h>
 
-OnInit { logger::info("Hello log from SKSE Starter Kit!"); }
-OnDataLoaded { ConsoleLog("Hello console from SKSE Starter Kit!"); }
-
 EventHandlers {
-    On<RE::TESActivateEvent>([](const RE::TESActivateEvent* event) {
-        auto activated = event->objectActivated->GetBaseObject()->GetName();
-        auto activator = event->actionRef->GetBaseObject()->GetName();
-        ConsoleLog("{} activated {}", activator, activated);
+    On<RE::TESContainerChangedEvent>([](const RE::TESContainerChangedEvent* event) {
+        if (event->newContainer == 0x14) {
+            auto* baseForm = RE::TESForm::LookupByID(event->baseObj);
+            logger::info("Player acquired {}", baseForm->GetName());
+        } else if (event->oldContainer == 0x14) {
+            auto* baseForm = RE::TESForm::LookupByID(event->baseObj);
+            logger::info("Player lost {}", baseForm->GetName());
+        }
     });
 }
